56bdf66637c6d05cfcfc13c9ac0cf44d3f413ad0
yyds <yyds@gmail.com>
2024-06-25 16:17:35 +0800

    add telegram info

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -12,9 +12,14 @@ function Options() {
                     <span style={{ fontSize: '0.75rem' }}>Jyutping for Cantonese</span>
                 </Typography>
                 <Divider />
-                <Stack sx={{ mt: '10px', height: '46.5px' }}>
+                <Stack sx={{ mt: '10px', height: '90.5px' }}>
                     <Typography variant="strong" color="primary" textAlign="center">
                         Author: Donnie Yang
+                    </Typography>
+                    <Typography color="primary" textAlign="center">
+                        <a style={{ fontSize: '0.75rem', color: '#2196f3' }} href="https://t.me/+RULUZ0XIMr73312r">
+                            Telegram Group
+                        </a>
                     </Typography>
                     <Typography color="primary" textAlign="center">
                         <a style={{ fontSize: '0.75rem', color: '#2196f3' }} href="mailto:dgutyang@gmail.com">
